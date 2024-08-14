79c7467c5f1948098644f4304fdc841cc77e9768
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-08-14 13:59:46 +0700

    Initial Commit

---
+++ TestFirebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestFirebase
+//
+//  Created by Natasha Radika on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestFirebase/TestFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFirebaseApp.swift
+//  TestFirebase
+//
+//  Created by Natasha Radika on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TestFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
